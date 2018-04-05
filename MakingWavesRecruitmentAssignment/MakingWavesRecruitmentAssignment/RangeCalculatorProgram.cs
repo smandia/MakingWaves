0ab8f9ff942ce634dcf023742e61469a2cb0f00f
smandia <sureshmandia@yahoo.in>
2018-04-05 15:10:49 -0700

    DateRangeCalculator

---
+++ MakingWavesRecruitmentAssignment/MakingWavesRecruitmentAssignment/RangeCalculatorProgram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+
+namespace MakingWavesRecruitmentAssignment
+{
+   public class RangeCalculatorProgram
+    {        
+        static void Main(string[] args)
+        {
+            if (!HasValidNumberOfArguments(args))
+            {
+                Console.WriteLine("Invalid number of input arguments");
+                return;
+            }
+
+            try
+            {   
+                Console.WriteLine(CalculateOutputFormat(ParseInput(args[0]), ParseInput(args[1])));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Incorrect input format - {0}",ex);
+            }            
+        }
+
+        public static bool HasValidNumberOfArguments(string[] args)
+        {
+            return args != null && args.Length == 2;
+        }
+
+        public static DateTime ParseInput(string dateArgument)
+        {
+            return DateTime.ParseExact(dateArgument, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        }
+
+        public static string CalculateOutputFormat(DateTime startDate, DateTime endDate)
+        {
+            var differentYearMsg = string.Format("{0}-{1}", startDate.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture), endDate.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture));
+            var differentMonthSameYearMsg = string.Format("{0}.{1}-{2}", startDate.Day.ToString("00"), startDate.Month.ToString("00"), endDate.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture));
+            var differentDaySameMonthYearMsg = string.Format("{0}-{1}", startDate.Day.ToString("00"), endDate.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture));
+            var sameDateMsg = string.Format(endDate.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture));
+            var contactSupportMsg = string.Format("Please contact support for such input {0}, {1}", startDate, endDate);
+
+            if(startDate > endDate)
+            {
+                return contactSupportMsg;
+            }
+
+            return startDate.Year != endDate.Year ? differentYearMsg
+                    : startDate.Month != endDate.Month ? differentMonthSameYearMsg
+                    : startDate.Day != endDate.Day ? differentDaySameMonthYearMsg
+                    : DateTime.Compare(startDate, endDate) == 0 ? sameDateMsg
+                    : contactSupportMsg;
+        }        
+    }
+}
---
+++ MakingWavesRecruitmentAssignment/MakingWavesRecruitmentAssignmentTests/RangeCalculatorProgramTests.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Globalization;
+using NUnit.Framework;
+using MakingWavesRecruitmentAssignment;
+
+namespace MakingWavesRecruitmentAssignmentTests
+{
+    [TestFixture]
+    public class RangeCalculatorProgramTests
+    {
+        [Test]
+        public void HasValidNumberOfArguments_CorrectNumberOfArgs_ReturnsTrue()
+        {
+            var inputArguments = new string[] {"a", "b" };
+            var result = RangeCalculatorProgram.HasValidNumberOfArguments(inputArguments);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void HasValidNumberOfArguments_CorrectNumberOfEmptyArgs_ReturnsTrue()
+        {
+            var inputArguments = new string[] { "", "" };
+            var result = RangeCalculatorProgram.HasValidNumberOfArguments(inputArguments);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void HasValidNumberOfArguments_InCorrectNumberOfArgs_ReturnsFalse()
+        {
+            var inputArguments = new string[] { "a" };
+            var result = RangeCalculatorProgram.HasValidNumberOfArguments(inputArguments);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void HasValidNumberOfArguments_NullArgs_ReturnsFalse()
+        {
+            string[] inputArguments = null;
+            var result = RangeCalculatorProgram.HasValidNumberOfArguments(inputArguments);
+            Assert.IsFalse(result);
+        }
+
+        [Test]
+        public void HasValidNumberOfArguments_CorrectNumberOfDateArgs_ReturnsTrue()
+        {
+            var inputArguments = new string[] { "01.01.2016", "05.02.2016" };
+            var result = RangeCalculatorProgram.HasValidNumberOfArguments(inputArguments);
+            Assert.IsTrue(result);
+        }
+
+        [TestCase("01.01.2016")]
+        [TestCase("25.01.2016")]        
+        public void ParseInput_CorrectDateArgs_ReturnsParsedDate(string inputArgument)
+        {
+            var result = RangeCalculatorProgram.ParseInput(inputArgument);
+            Assert.IsInstanceOf<DateTime>(result);
+        }
+
+        [TestCase("30.02.2018")]
+        [TestCase("01.01.20168")]
+        [TestCase("abc")]
+        [TestCase("-10.02.2018")]
+        [TestCase("10.13.2018")]
+        [TestCase("10/11/2018")]
+        public void ParseInput_InCorrectDateArgs_ReturnsException(string inputArgument)
+        {
+             Assert.Throws<FormatException>(() => RangeCalculatorProgram.ParseInput(inputArgument));
+        }
+
+        [Test]
+        public void CalculateOutputFormat_StartDateGreaterThenEndDate_ErrorMessage()
+        {
+            var startDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact("15.01.2016", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            var result = RangeCalculatorProgram.CalculateOutputFormat(startDate, endDate);
+            StringAssert.Contains("Please contact support", result);
+        }
+
+        [Test]
+        public void CalculateOutputFormat_StartDateYearDifferentEndDateYear_DisplayMessage()
+        {
+            var startDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact("15.09.2018", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            var result = RangeCalculatorProgram.CalculateOutputFormat(startDate, endDate);
+            StringAssert.Contains("01.02.2017-15.09.2018", result);
+        }
+
+        [Test]
+        public void CalculateOutputFormat_StartDateMonthDifferentEndDateMonth_DisplayMessage()
+        {
+            var startDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact("15.03.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            var result = RangeCalculatorProgram.CalculateOutputFormat(startDate, endDate);
+            StringAssert.Contains("01.02-15.03.2017", result);
+        }
+
+        [Test]
+        public void CalculateOutputFormat_StartDateDayDifferentEndDateDay_DisplayMessage()
+        {
+            var startDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact("15.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            var result = RangeCalculatorProgram.CalculateOutputFormat(startDate, endDate);
+            StringAssert.Contains("01-15.02.2017", result);
+        }
+
+        [Test]
+        public void CalculateOutputFormat_StartDateSameEndDate_DisplayMessage()
+        {
+            var startDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact("01.02.2017", "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+            var result = RangeCalculatorProgram.CalculateOutputFormat(startDate, endDate);
+            StringAssert.Contains("01.02.2017", result);
+        }
+    }
+}
